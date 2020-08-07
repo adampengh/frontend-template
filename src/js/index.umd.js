899fc2db739edacf67ca4e05ed0602d7bb04ba56
Adam Pengh <adam.pengh@ingka.ikea.com>
2020-08-07 11:06:56 -0400

    add handlebars library

--- src/js/index.umd.js
+++ src/js/index.umd.js
@@ -1,17 +1,8 @@
-import pkg from '../../package.json';
 import Currency from './library/util/currency';
 
-// const logoNode = document.querySelector('.hnf-header__logo img');
-// if (logoNode) {
-//     logoNode.setAttribute('src', 'https://via.placeholder.com/100x40/ccc/333?text=LOGO');
-// }
-
-// const productsLink = document.querySelector('.hnf-header__nav > ul > li:first-child > a');
-// productsLink.textContent = `Version: ${pkg.version}`;
-
 // Assign modules to window.ikea global object
-window.ikea = window.ikea || {};
-window.ikea.frontend = {
+window.library = window.library || {};
+window.library.frontend = {
     Currency
 };
 
