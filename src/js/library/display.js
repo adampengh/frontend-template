758ed1d1d45c56159c20e6557c19a5416b3855e4
Adam Pengh <adam.pengh@ingka.ikea.com>
2020-08-20 14:32:27 -0400

    updates to setup

--- src/js/library/display.js
+++ src/js/library/display.js
@@ -10,9 +10,9 @@ Display.hideNode = (target) => {
 /**
  * Inserts a node after a target node
  * @param {!Element} target Node we want to insert after.
- * @param {!Node} node Node to insert.
+ * @param {!Node} element Node to insert.
  */
-Display.insertAfter = (target, node) => {
+Display.insertAfter = (target, element) => {
     let nextSibling = target.nextSibling;
     let nextValidSibling;
     while (nextSibling) {
@@ -24,9 +24,9 @@ Display.insertAfter = (target, node) => {
     }
 
     if (nextValidSibling) {
-        target.parentNode.insertBefore(node, nextValidSibling);
+        target.parentNode.insertBefore(element, nextValidSibling);
     } else {
-        target.parentNode.appendChild(node);
+        target.parentNode.appendChild(element);
     }
 };
 
@@ -34,41 +34,41 @@ Display.insertAfter = (target, node) => {
 /**
  * Inserts a node before a target node
  * @param {!Element} target Node we want to insert before
- * @param {!Node} node Node to insert
+ * @param {!Node} element Node to insert
  */
-Display.insertBefore = (target, node) => {
-    target.parentNode.insertBefore(node, target);
+Display.insertBefore = (target, element) => {
+    target.parentNode.insertBefore(element, target);
 };
 
 
 /**
  * Inserts a node as the first child of a target node
  * @param {!Element} target Node we want to insert child into
- * @param {!Node} node Node to insert
+ * @param {!Node} element Node to insert
  */
-Display.insertFirstChild = (target, node) => {
-    target.insertBefore(node, target.firstChild);
+Display.insertFirstChild = (target, element) => {
+    target.insertBefore(element, target.firstChild);
 };
 
 
 /**
  * Inserts a node as the last child of a target node
  * @param {!Element} target Node we want to insert child into
- * @param {!Node} node Node to insert
+ * @param {!Node} element Node to insert
  */
-Display.insertLastChild = (target, node) => {
-    target.appendChild(node);
+Display.insertLastChild = (target, element) => {
+    target.appendChild(element);
 };
 
 
 /**
  * Hides the target element and insert the new element after the target element
  * @param {!Element} target Target element
- * @param {!Node} node Replacement element
+ * @param {!Node} element Replacement element
  */
-Display.insertReplace = (target, node) => {
+Display.insertReplace = (target, element) => {
     Display.hideNode(target);
-    Display.insertAfter(target, node);
+    Display.insertAfter(target, element);
 };
 
 
@@ -76,12 +76,12 @@ Display.insertReplace = (target, node) => {
  * Insert the replacement node as a child of the target element
  * Moves all of the existing chilc element(s) to a hidden div as the first child of the body
  * @param {!Element} target Target element
- * @param {!Node} node Replacement element
+ * @param {!Node} element Replacement element
  */
-Display.insertTakeover = (target, node) => {
+Display.insertTakeover = (target, element) => {
     if (document.body) {
         const hiddenDiv = document.createElement('div');
-        hiddenDiv.id = 'beachhouse_hidden_takeover';
+        hiddenDiv.id = 'frontend_hidden_takeover';
         hiddenDiv.style.display = 'none';
 
         let child;
@@ -89,7 +89,7 @@ Display.insertTakeover = (target, node) => {
             hiddenDiv.appendChild(child);
         }
 
-        Display.insertFirstChild(target, node);
+        Display.insertFirstChild(target, element);
         Display.insertFirstChild(document.body, hiddenDiv);
     }
 };
@@ -107,38 +107,17 @@ Display.INSERT_METHODS = {
 
 /**
  * Inserts a node using an insertion method relative to a target node
- * @param {Node} node The node we are inserting
+ * @param {Node} element The node we are inserting
  * @param {Node} target The target element
  * @param {string} insertMethod The insertion method
  */
-Display.insertNode = (node, target, insertMethod) => {
+Display.insertNode = (element, target, insertMethod) => {
     console.group('Display.insertNode');
 
     const insert = Display.INSERT_METHODS[insertMethod];
-    insert(target, node);
+    insert(target, element);
 
     console.groupEnd();
-};
-
-
-/**
- * Inserts an image relative to a target element
- * @param {Element} targetNode The target element
- * @param {string} insertMethod Insertion method
- * @param {string} src Image src attribute
- * @param {string} alt Alt text attribute
- * @param {string=} opt_title Optional title attribute
- */
-Display.insertImage = (targetNode, insertMethod, src, alt, opt_title) => {
-    const img = document.createElement('img');
-    img.src = src;
-    img.setAttribute('alt', alt);
-    if (opt_title) {
-        img.setAttribute('title', opt_title);
-    }
-
-    // Insert the image
-    Display.insertNode(img, targetNode, insertMethod);
 };
 
 
@@ -164,6 +143,27 @@ Display.insertHTML = (targetNode, insertMethod, html) => {
     const element = Display.createElementFromHTML(html);
     // Insert the HTML
     Display.insertNode(element, targetNode, insertMethod);
+};
+
+
+/**
+ * Inserts an image relative to a target element
+ * @param {Element} targetNode The target element
+ * @param {string} insertMethod Insertion method
+ * @param {string} src Image src attribute
+ * @param {string} alt Alt text attribute
+ * @param {string=} opt_title Optional title attribute
+ */
+Display.insertImage = (targetNode, insertMethod, src, alt, opt_title) => {
+    const img = document.createElement('img');
+    img.src = src;
+    img.setAttribute('alt', alt);
+    if (opt_title) {
+        img.setAttribute('title', opt_title);
+    }
+
+    // Insert the image
+    Display.insertNode(img, targetNode, insertMethod);
 };
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ import Dom from './library/dom';
 import Util from './library/util';
 
 // Replace the current IKEA header logo with a new placeholder image
-const logo = document.querySelector('.hnf-header__logo a');
+const logo = document.querySelector('.hnf-header__logo img');
 if (logo) {
     Display.insertImage(logo, 'https://via.placeholder.com/100x40?text=LOGO', 'LOGO', 'NEW LOGO');
 }
